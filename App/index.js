0e4f7bc12f94c55d3216a2e1b228ee2f2a225527
EL KAMEL <yelkamel@gmail.com>
2017-10-13 17:34:19 +0200

    Clean code

--- App/index.js
+++ App/index.js
@@ -157,29 +157,3 @@ var styles = StyleSheet.create({
     fontSize: 18,
   },
 })
-
-/*
-DYNAMIC HEADER
-
-Dynamic Header with TranslateY (moving view) and nativeDrive (animated Component)
-https://medium.com/appandflow/react-native-collapsible-navbar-e51a049b560a
-=> height static issue with child view 
-
-Dynamic Header with Heigh dynamic (can't use nativeDrive with Height style property)
-https://medium.com/appandflow/react-native-scrollview-animated-header-10a18cb9469e
-=> Android version lag when scrolling slow
-==> solution: onScroll Event give to much precision for the rendering slowing down the transformation fix the issue.
-*/
-
-/*
-SWIPER INDIDE SWIPER
-
-Gesture collision between parent and child swiper fix by using Swiper Package inside TabView
-todo: Make an header Tab for both pages
-*/
-
-/*
-COLLAPSIBLE LIST
-
-Using FlatList component (better version of ListView)
-*/
